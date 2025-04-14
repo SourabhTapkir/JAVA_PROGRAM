759e3e546b7744ba37dd8f6a9962068950e49565
SourabhTapkir <84170131+SourabhTapkir@users.noreply.github.com>
2025-04-14 23:45:55 +0530

    Add files via upload

---
+++ Stars_patterns/SolidRectangleSP.java
@@ -0,0 +1,26 @@
+package Stars;
+//import java.util.Scanner;
+public class SolidRectangleSP {
+	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		System.out.println("Enter the number of Rows:");
+//		int n = scan.nextInt();
+//		
+//		System.out.println("Enter the number of Columns:");
+//		int m = scan.nextInt();
+		
+		int n;
+		int m;
+
+		///outer Loops
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+	}
+
+}
