6a2c5573517e022b2534d170570fe01990644f35
SourabhTapkir <84170131+SourabhTapkir@users.noreply.github.com>
2025-04-28 23:52:57 +0530

    Add files via upload
    Very Basic of Array

---
+++ arrays/Arrayss.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Arrayss {
+	public static void main(String[] args) {
+		int Num[] = {16,7,2,9};
+	//	Num[3] = 4; ///to replace or change or update the values of a particular index here at third index the value 4 is updated
+	//	System.out.println(Num[3]); //// Fetches the Value of a particular index number 
+		
+		for(int i=0;i<4;i++) {
+			System.out.println(Num[i]);
+		}
+		
+	}
+
+}
